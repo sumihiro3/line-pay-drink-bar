41484ee5ffadc076449fff88341ddc4cda3e1019
sumihiro3 <sumihiro@gmail.com>
2020-04-18 14:46:25 +0900

    add vConsole

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,11 +12,35 @@
 </template>
 
 <script>
+import consola from 'consola'
 export default {
   data() {
     return {
       fixed: false,
       title: 'LINE Pay Drink Bar'
+    }
+  },
+  mounted() {
+    consola.log('USE_VCONSOLE', process.env.USE_VCONSOLE)
+    if (process.env.USE_VCONSOLE) {
+      consola.log('Initializing vConsole')
+      this.initVconsole()
+    }
+  },
+  methods: {
+    initVconsole() {
+      // vConsoleをイニシャライズ
+      /* eslint no-unused-vars: 0 */
+      const vconsole = new window.VConsole({
+        defaultPlugins: ['system', 'network', 'element', 'storage'],
+        maxLogNumber: 1000,
+        onReady() {
+          console.log('vConsole is ready.')
+        },
+        onClearLog() {
+          console.log('on clearLog')
+        }
+      })
     }
   }
 }
