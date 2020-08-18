4ccdfc0451ed904c07e9fe82ea9d8418b8d10654
sumihiro3 <sumihiro@gmail.com>
2020-08-18 13:46:43 +0900

    use default credential

--- server/pay.js
+++ server/pay.js
@@ -22,11 +22,12 @@ const obnizApiToken = process.env.OBNIZ_API_TOKEN
 
 // Initialize firebase-admin
 admin.initializeApp({
-  credential: admin.credential.cert({
-    "project_id": process.env.FIREBASE_PROJECT_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-  }),
+  // credential: admin.credential.cert({
+  //   "project_id": process.env.FIREBASE_PROJECT_ID,
+  //   "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  //   "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  // }),
+  credential: admin.credential.applicationDefault(),
   databaseURL: process.env.FIREBASE_DATABASE_URL
 });
 const database = admin.firestore()
