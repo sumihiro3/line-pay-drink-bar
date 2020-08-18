c6b692d680ce157d5cb9931be46c161482f57d11
sumihiro3 <sumihiro@gmail.com>
2020-08-18 14:15:12 +0900

    change content-type

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,7 @@
 export default function({ $axios, redirect }) {
   $axios.onRequest((config) => {
     config.headers.common['Access-Control-Allow-Origin'] = '*'
-    config.headers.common['Content-Type'] = 'application/json'
+    config.headers.common['Content-Type'] = 'application/x-www-form-urlencoded'
     config.headers.common['X-Requested-With'] = 'XMLHttpRequest'
   })
   $axios.onError((error) => {
